e20b72d434969fa12eb64123c311d46f65277201
mariagrazianuzzolese05-svg <mariagrazianuzzolese05@gmail.com>
2026-02-24 17:01:19 +0100

    np

--- mariagrazia.py
+++ mariagrazia.py
@@ -1,3 +1,13 @@
 import numpy as np
 dati_caricati = np.loadtxt("analisidigruppo.txt")
 print(dati_caricati)
+
+'''Analisi posizionale (ricerche, argmin, argmax, percentili)
+Si possono analizzare le posizioni relative dei valori:
+np.argmin(arr) → indice del valore minimo
+np.argmax(arr) → indice del valore massimo
+np.percentile(arr, 50) → mediana
+np.searchsorted(arr, x) → trovare posizione ordinata di inserimento
+'''
+
+minimo=np.argmin(dati_caricati)
