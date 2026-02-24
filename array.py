6845705b0206b938d4d4f4103acf9626e826f1a4
mariagrazianuzzolese05-svg <mariagrazianuzzolese05@gmail.com>
2026-02-24 16:59:16 +0100

    np

---
+++ array.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+nome_file = "analisidigruppo2.txt"
+
+# 1. CREAZIONE DEL FILE (Solo se non esiste, per evitare l'errore)
+# Creiamo una matrice finta per simulare dei dati di laboratorio
+arr=np.random.randint(1,10, size=5)
+#matrice2=np.random.rand(4,4)
+np.savetxt(nome_file, arr) 
+print(f"File '{nome_file}' creato con dati di prova!")
+
+with open(nome_file, "r") as file:
+    righe = file.readlines()
+    print(f"Letto correttamente! Il file ha {len(righe)} righe.")
--- giovanni.py
+++ giovanni.py
@@ -1,3 +1,6 @@
 import numpy as np
 dati_caricati = np.loadtxt("analisidigruppo.txt")
 print(dati_caricati)
+
+dati_caricati2 = np.loadtxt("analisidigruppo2.txt")
+print(dati_caricati2)
