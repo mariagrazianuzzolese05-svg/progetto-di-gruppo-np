366bba14dc3b0bdb50b7db3da68450bc9ce2db14
mariagrazianuzzolese05-svg <mariagrazianuzzolese05@gmail.com>
2026-02-24 16:40:51 +0100

    np

--- matrice.py
+++ matrice.py
@@ -1,3 +1,7 @@
 import numpy as np
 matrice1=np.random.rand(4,4)
 matrice2=np.random.rand(4,4)
+with open("lavorodigruppo.txt") as file:
+        
+        fileite(f"arr: {matrice}\n")
+        f.write(f"sottomatrice: {sotto_matrice}\n")
